d2b19b2e8dff0ef4ac6d69bf0262f127a5f18346
carballod <109915531+carballod@users.noreply.github.com>
2023-03-27 21:12:18 -0300

    Agrega el custom hook useTodo y mejora la legibilidad del codigo

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,57 +1,16 @@
-
-import { useEffect, useReducer } from 'react';
 import { TodoAdd } from './TodoAdd';
 import { TodoList } from './TodoList';
-import { todoReducer } from './todoReducer';
+import { useTodo } from './hooks/useTodo';
 
-export const initialState = [
-    // {
-    //     id: new Date().getTime(),
-    //     description: 'Recolectar la piedra del alma',
-    //     done: false,
-    // },
-]
-
-const init = () => {
-    return JSON.parse( localStorage.getItem( 'todos' ) ) || [];
-}
 
 export const TodoApp = () => {
 
-    const [todos, dispatch] = useReducer( todoReducer, initialState, init );
-
-    useEffect(() => {
-        localStorage.setItem( 'todos', JSON.stringify( todos ) );
-    }, [todos])
-    
-
-    const handleNewTodo = ( todo ) => {
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo,
-        };
-
-        dispatch( action );
-    }
-
-    const handleDeleteTodo = ( id ) => {
-        dispatch( {
-            type: '[TODO] Remove Todo',
-            payload: id,
-        } );
-    }
-
-    const handleToggleTodo = ( id ) => {
-        dispatch({
-            type: '[TODO] Toggle Todo',
-            payload: id,
-        })
-    }
+    const { todos, todosCount, pendingTodosCount, handleDeleteTodo, handleToggleTodo, handleNewTodo } = useTodo();
 
 
     return (
         <>
-            <h1>Todo App: 10, <small>pendientes: 2</small></h1>
+            <h1>Todo App: { todosCount }, <small>pendientes: { pendingTodosCount }</small></h1>
             <hr />
 
             <div className="row">
